15365b16da57a9a97de598e0fdf64e061a00d08d
bone1nis <bone1nis@gmail.com>
2025-01-16 12:50:54 +0300

    delete http hook

--- src/store/catsSlice.ts
+++ src/store/catsSlice.ts
@@ -2,18 +2,31 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from ".";
 
-import { useHttp } from "../hooks/http.hook";
-
 import { ICat } from "../types/types";
 
 
 export const fetchCats = createAsyncThunk<ICat[], number | undefined>(
     "cats/fetchCats",
     async (value = 20) => {
-        const request = useHttp<ICat[]>();
+        let data;
 
-        const res = await request(`https://api.thecatapi.com/v1/images/search?limit=${value}&api_key=live_xZleH3vcw7GdeK3M3k5rgc4P49RMgyM5ZF59otRlTbRyM2qDwu5DL0Qs8ypFmH2I`);
-        return res
+        try {
+            const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=${value}&api_key=live_xZleH3vcw7GdeK3M3k5rgc4P49RMgyM5ZF59otRlTbRyM2qDwu5DL0Qs8ypFmH2I`);
+
+            if (!response.ok) {
+                throw new Error(`Could not fetch, status: ${response.status}`);
+            }
+
+            data = await response.json();
+        } catch(e: unknown) {
+            if (e instanceof Error) {
+                throw new Error(`HTTP request failed: ${e.message}`);
+            } else {
+                throw new Error(`Failed`);
+            }
+        }
+
+        return data;
     }
 );
 
